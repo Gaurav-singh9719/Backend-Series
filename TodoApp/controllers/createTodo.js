016cc7e3cb386a219ae30f25aa56969b0d11dbc2
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-08-24 20:55:26 +0530

    update in createTodo.js File

--- TodoApp/controllers/createTodo.js
+++ TodoApp/controllers/createTodo.js
@@ -1,34 +1,27 @@
-// import th model
+// import the model
+const Todo = require("../models/todo");
 
-const Todo = require("../models/todo")
+// define route handler
+exports.createTodo = async (req, res) => {
+  try {
+    // extract title and description from request body
+    const { title, description } = req.body;
 
-//  define route handler
+    // create a new todo object and insert in DB
+    const response = await Todo.create({ title, description });
 
-exports.createTodo = async(req, res) =>{
-    try {
-        // extract title and description from request body
-        const {title, description} = req.body;
-
-        // create a new todo object and insert in DB
-
-        const response = await Todo.create(title, description);
-        // send a json response with a success flag
-
-        res.status(200).json(
-            {
-                success:true,
-                data:response,
-                message: "Entry created Successfully"
-            }
-        )
-    } catch (error) {
-        console.error(error);
-        console.log(error);
-        res.status(500)
-        .json({
-            success:false,
-            data:"internal Server error",
-            message:error.message,
-        })
-    }
-}
+    // send a json response with a success flag
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "Entry created Successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      data: "internal Server error",
+      message: error.message,
+    });
+  }
+};
