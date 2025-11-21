e36d96d357d4aacda2f113b575072b07d5a0e238
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-11-21 12:28:36 +0530

    FileUploader completed

--- fileUpload/models/File.js
+++ fileUpload/models/File.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+const nodemailer = require("nodemailer");
 const fileSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -13,6 +13,37 @@ const fileSchema = new mongoose.Schema({
     },
     email:{
         type:String,
+    }
+});
+
+// post middleware
+fileSchema.post("save", async function(doc){
+    try {
+        console.log("DOC", doc);
+
+        // transporter
+        // todo: shift this configuration under / config folder
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        // send mail
+        let info = await transporter.sendMail({
+            from:`Gaurav Singh`,
+            to: doc.email,
+            subject: "New File Uploaded on Cloudinary",
+                html:`<h2> Hello Jii </h2> <p>File Uploaded view here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`,
+        })
+
+        console.log("Info", info);
+
+    } catch (error) {
+        console.error(error);
+
     }
 });
 
